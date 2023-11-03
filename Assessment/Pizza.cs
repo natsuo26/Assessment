59b4a01051dcc6e0e8fa5e592c40a402a33569c4
u31d81 <u31d81@INGURWN130183>
2023-11-03 10:19:08 +0530

    Add project files.

---
+++ Assessment/Pizza.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    internal class Pizza
+    {
+        string size;
+        public string Size {  get { return size; } set {  size = value; } }
+        int qtyCheeseToppings;
+        public int QtyCheeseToppings { get { return qtyCheeseToppings; } set {  qtyCheeseToppings = value;} }
+        int qtyPepperoniToppings;
+        public int QtyPepperoniToppings {  get { return qtyPepperoniToppings; } set { qtyPepperoniToppings = value; } }
+
+        int qtyHamToppings;
+        public int QtyHamToppings { get { return qtyHamToppings; } set{ qtyHamToppings = value; } }
+
+        public Pizza()
+        {
+            size = "";
+            qtyCheeseToppings = 0;
+            qtyPepperoniToppings = 0;
+            qtyHamToppings = 0;
+        }
+        public Pizza(string size, int qtyCheeseToppings ,int qtyPepperoniToppings,int qtyHamToppings)
+        {
+            this.size = size;
+            this.qtyCheeseToppings= qtyCheeseToppings;
+            this.qtyHamToppings = qtyHamToppings;
+            this.qtyPepperoniToppings= qtyPepperoniToppings;
+
+        }
+        public double CalcCost()
+        {
+            double cost = 0.0;
+            switch (size.ToLower())
+            {
+                case "small":
+                    {
+                        cost = 10 + 2*(qtyCheeseToppings + qtyPepperoniToppings + qtyHamToppings);
+                        break;
+                    }
+                case "medium":
+                    {
+                        cost = 12 + 2 * (qtyCheeseToppings + qtyPepperoniToppings + qtyHamToppings);
+                        break;
+                    }
+                case "large":
+                    {
+                        cost = 14 + 2 * (qtyCheeseToppings + qtyPepperoniToppings + qtyHamToppings);
+                        break;
+                    }
+
+            }
+                    return cost;
+        }
+
+        public string GetDescription()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Pizza size : {size}");
+            sb.AppendLine($"number of  cheese toppings : {qtyCheeseToppings}");
+            sb.AppendLine($"number of  pepperoni toppings : {qtyPepperoniToppings}");
+            sb.Append($"number of  Ham toppings : {qtyHamToppings}");
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ Assessment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+
+namespace Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pizza> pizzaList = new List<Pizza>();
+            string choice = "y";
+            while (choice.ToLower() == "y")
+            {
+                
+                string size=null;
+                int qtyCheese, qtyPepperoni, qtyHam;
+                Console.WriteLine("Enter size: ");
+                try
+                {
+                    size = Console.ReadLine();
+                    if (size.ToLower()!="small" && size.ToLower()!="medium"&&size.ToLower()!="large")
+                    {
+                        throw new Exception("Please enter size: small, medium, large");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                Console.WriteLine("quantity of cheese on you pizza?");
+                qtyCheese = byte.Parse(Console.ReadLine());
+                Console.WriteLine("quantity of pepperoni on you pizza?");
+                qtyPepperoni=byte.Parse(Console.ReadLine()); 
+                Console.WriteLine("quantity of Ham on you pizza?");
+                qtyHam=byte.Parse(Console.ReadLine());
+                Pizza pizza=new Pizza(size,qtyCheese,qtyPepperoni,qtyHam);
+                pizzaList.Add(pizza);
+
+                Console.WriteLine("do you want to add another Pizza?\nyes(y) no(press any other key)");
+                choice = Console.ReadLine();
+            }
+
+            foreach(Pizza pizza in pizzaList)
+            {
+                string desc=pizza.GetDescription();
+                Console.WriteLine(desc);
+                double cost = pizza.CalcCost();
+                Console.WriteLine($"Cost of this pizza is {cost}\n");
+            }
+        }
+    }
+}
